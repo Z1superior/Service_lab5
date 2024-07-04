5be0ba9f9caf36c63736c5a93f99dbb5d56a22d9
张浩芳 <2541764293@qq.com>
2024-07-05 02:32:59 +0800

    first commit

---
+++ Service_Consumer_16000/src/main/java/com/example/ConsumerApplication16000.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import com.example.rule.CustomThreeTimeLoadBalancer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@SpringBootApplication
+@EnableFeignClients
+@LoadBalancerClient(name="provider-server",configuration = CustomThreeTimeLoadBalancer.class)
+public class ConsumerApplication16000 {
+//    /**
+//     * 使用spring提供的RestTemplate发送http请求调用微服务
+//     */
+//    @Bean
+//    @LoadBalanced
+//    public RestTemplate getRestTemplate(){
+//        return  new RestTemplate();
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication16000.class,args);
+    }
+}
